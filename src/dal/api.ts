7fb0b6f96901837f0b9cc3a87364b931f6317d7a
Karinka_ter <tigrantervartanyan@gmail.com>
2025-11-26 13:41:05 +0300

    add env for api-key

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -1,19 +1,23 @@
 import type {SelectedTrackType, TrackType} from "../Types.ts";
 
+const prerareHeaders = ()=>{
+    const apiKey =  import.meta.env.VITE_API_KEY
+    if(!apiKey) return undefined
+    return {
+        'api-key': apiKey
+    }
+}
+
 
 export const getTrack = async (selectedTrackId:string): Promise<{data:SelectedTrackType}>=>{
    return fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${selectedTrackId}`,{
-        headers:{
-         //   'api-key': '2f030ac2-1bcc-4540-8cde-e88ef93a721b'
-        }
+        headers: prerareHeaders()
     }).then(res => res.json())
 
 }
 
 export const getTracksViaAPI = async ():Promise<{data:TrackType[]}>=>{
    return fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
-        headers: {
-         //   'api-key': '2f030ac2-1bcc-4540-8cde-e88ef93a721b'
-        }
+        headers: prerareHeaders()
     }).then(res => res.json())
 }
